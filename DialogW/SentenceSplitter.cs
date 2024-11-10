cb9330e13c22bad3cd29fe954814a9d284ee62db
Acrux <acruxstudios.dev@gmail.com>
2024-11-10 15:51:52 -0300

    Made ellipsis sentence breaker (when followed by a space)

--- DialogW/SentenceSplitter.cs
+++ DialogW/SentenceSplitter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace DialogW
@@ -8,8 +9,8 @@ namespace DialogW
 		private const string PATTERN =
 			@"(?<!\b(?:Mr|Mrs|Ms|Dr|St|Jr|Sr|vs|etc|i\.e|e\.g))   # Avoid split after common abbreviations
 			  (?<!\b[A-Z]\.)                                      # Avoid split after initials (e.g., 'J. K. Rowling')
-			  (?<=\.|\?|!)                                        # Ensure the sentence ends with '.', '?' or '!'
-			  (?=\s+[A-Z])                                        # Followed by whitespace and a capital letter";
+			  (?<=\.{3}|\.|\?|!)                                  # Ensure the sentence ends with '...', '.', '?' or '!'
+			  (?=\s+[a-zA-Z])                                     # Followed by whitespace and a letter";
 
 		private readonly static Regex regex;
 
