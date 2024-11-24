bafcf99786c789c76eb407584b155a1f58510e3a
Acrux <acruxstudios.dev@gmail.com>
2024-11-24 20:54:23 -0300

    Removed dead code in Program

--- DialogW/Program.cs
+++ DialogW/Program.cs
@@ -17,25 +17,17 @@ public static class FileReaderService
         };
 
         string fullText = File.ReadAllText("text.txt").Replace("\r\n", "\n");
-        string contentSlice = "";
-        int currentIndex = 0;
-
-        Random random = new Random();
-        while (contentSlice != fullText)
+        string textSlice = "";
+        for(int i = 0; i < fullText.Length; ++i)
         {
-            int previousIndex = currentIndex;
-            //int increment = (random.Next(fullText.Length - currentIndex + 1) / 2);
-            int increment = 1;
-            increment = (increment == 0) ? 1 : increment;
-            currentIndex += increment;
-            contentSlice += fullText.Substring(previousIndex, currentIndex - previousIndex);
-            writter.UpdateContent(contentSlice);
-            await Task.Delay(2);
-        }
-        writter.FinalizeWritting();
-        
-        Console.WriteLine();
-        Console.WriteLine(writter.ActiveDialogue);
+            textSlice += fullText[i];
+			writter.UpdateContent(textSlice);
+			await Task.Delay(2);
+		}
+		writter.FinalizeWritting();
+
+		Console.WriteLine();
+		Console.WriteLine(writter.ActiveDialogue);
     }
 }
 
